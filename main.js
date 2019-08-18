a034d01f76d6eb928235c65e3680968565874c59
pyotr_chan <pollyfeb@outlook.com>
2019-08-18 11:54:58 +0900

    all done

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-      // 使用するデータ
-    },
-    methods: {
-      // 使用するメソッド
-    }
-  })
-
-// https://jp.vuejs.org/v2/examples/todomvc.html
 var STORAGE_KEY = 'todos-vuejs-demo'
 var todoStorage = {
   fetch: function() {
@@ -27,9 +16,64 @@ var todoStorage = {
 }
 
 new Vue({
-  // ...
+  el: '#app',
+  
+data: {
+      // ★STEP5 localStorage から 取得した ToDo のリスト
+    todos: [],
+    // ★STEP11 抽出しているToDoの状態
+    current: -1,
+  options: [
+    { value: -1, label: 'すべて' },
+    { value: 0,  label: '作業中' },
+    { value: 1,  label: '完了' }
+  ],
+  // 選択している options の value を記憶するためのデータ
+  // 初期値を「-1」つまり「すべて」にする
+  current: -1
+},
+  
+ // ★STEP12
+ computed: {
+ computedTodos: function() {
+      // データ current が -1 ならすべて
+      // それ以外なら current と state が一致するものだけに絞り込む
+    return this.todos.filter(function(el) {
+    return this.current < 0 ? true : this.current === el.state
+    }, this)
+  },
+  // ★STEP13 作業中・完了のラベルを表示する
+  labels() {
+     return this.options.reduce(function (a, b) {
+     return Object.assign(a, { [b.value]: b.label })
+     }, {})
+      // キーから見つけやすいように、次のように加工したデータを作成
+      // {0: '作業中', 1: '完了', -1: 'すべて'}
+    }
+  },
+
+    //step8
+  watch: {
+    // オプションを使う場合はオブジェクト形式にする
+    todos: {
+      // 引数はウォッチしているプロパティの変更後の値
+      handler: function(todos) {
+        todoStorage.save(todos)
+      },
+      // deep オプションでネストしているデータも監視できる
+      deep: true
+    }
+  },
+    
+ // step9
+   created() {
+    // インスタンス作成時に自動的に fetch() する
+    this.todos = todoStorage.fetch()
+  },
+  
   methods: {
-    // ToDo 追加の処理
+    
+    // ★STEP7 ToDo 追加の処理
     doAdd: function(event, value) {
       // ref で名前を付けておいた要素を参照
       var comment = this.$refs.comment
@@ -47,17 +91,19 @@ new Vue({
       })
       // フォーム要素を空にする
       comment.value = ''
-    }
-  },
-  watch: {
-    // オプションを使う場合はオブジェクト形式にする
-    todos: {
-      // 引数はウォッチしているプロパティの変更後の値
-      handler: function(todos) {
-        todoStorage.save(todos)
-      },
-      // deep オプションでネストしているデータも監視できる
-      deep: true
+    },
+    
+    // ★STEP10 状態変更の処理
+    doChangeState: function(item) {
+      item.state = item.state ? 0 : 1
+    },
+    
+    // ★STEP10 削除の処理
+
+    doRemove: function(item) {
+      var index = this.todos.indexOf(item)
+      this.todos.splice(index, 1)
     }
   }
+ 
 })
